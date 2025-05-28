3063f437bd68c7cd16dc806ba8e4619ed3c1d6ff
Janath Gunarathna <95167638+JanathGunarathna@users.noreply.github.com>
2025-05-28 14:37:13 +0530

    Initial FPY project

---
+++ Backend/src/main/java/com/fpy/Final/Year/Project/FinalYearProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fpy.Final.Year.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalYearProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalYearProjectApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/fpy/Final/Year/Project/FinalYearProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpy.Final.Year.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalYearProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
