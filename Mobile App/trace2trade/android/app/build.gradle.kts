e25762a50a950b269c6d1621d811f684101184a9
Janath Gunarathna <95167638+JanathGunarathna@users.noreply.github.com>
2025-07-15 00:31:19 +0530

    updated

--- Mobile App/trace2trade/android/app/build.gradle.kts
+++ Mobile App/trace2trade/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     namespace = "com.example.trace2trade"
    // compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
-    compileSdkVersion ='33'
+    compileSdk = 35
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -25,10 +25,10 @@ android {
         applicationId = "com.example.trace2trade"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
